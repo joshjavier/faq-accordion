ce2b3c0286baa02cfcf329c3767c6241aaca1534
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-01-02 20:22:33 +0800

    Add container markup and styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ import './App.css';
 function App() {
   return (
     <main>
-      <h1>
-        <IconStar />
-        <abbr title="Frequently Asked Questions">FAQs</abbr>
-      </h1>
-      <ul role="list">
-        {faqs.map((faq) => (
-          <li key={faq.id}>
-            <Accordion faq={faq} />
-          </li>
-        ))}
-      </ul>
+      <div className="container">
+        <h1>
+          <IconStar />
+          <abbr title="Frequently Asked Questions">FAQs</abbr>
+        </h1>
+        <ul role="list">
+          {faqs.map((faq) => (
+            <li key={faq.id}>
+              <Accordion faq={faq} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
