8245f481104e002dbf0d8f13c0b835012445219e
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-01-02 16:34:13 +0800

    Update main heading styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Accordion from './components/Accordion';
+import IconStar from './icons/IconStar';
 import { faqs } from './data.json';
 import './App.css';
 
 function App() {
   return (
     <main>
-      <h1 className="visually-hidden">FAQ accordion</h1>
+      <h1>
+        <IconStar />
+        <abbr title="Frequently Asked Questions">FAQs</abbr>
+      </h1>
       <ul role="list">
         {faqs.map((faq) => (
           <li key={faq.id}>
